653c0ca8841f73dbe6bdf299221ce4ccba4a1a4d
gavboi <gj1087@hotmail.com>
2023-03-14 10:25:52 -0400

    ui functinoal, creation still glitchy

--- ui.py
+++ ui.py
@@ -7,33 +7,60 @@ Contains:
 """
 
 import PySimpleGUI as psg
+import create
 
 
 # set theme
 psg.theme('DarkBrown1')
 
 # define window elements
-L_F1 = [[psg.Text('Preview Image/Frame 1')],
-          [psg.Input(), psg.FileBrowse()],
-          [psg.OK(), psg.Cancel()]]
+L_F1 = [[psg.Frame('Hidden Image/Frame 1', [
+          [psg.Input(k='f1'), psg.FileBrowse()]
+        ])]]
 """UI elements for selecting frame 1."""
 L_F2 = [[psg.Frame('Hidden Image/Frame 2', [
-          [psg.Input(), psg.FileBrowse()]
+          [psg.Input(k='f2'), psg.FileBrowse()]
        ])]]
 """UI elements for selecting frame 2."""
-L_GO = [[psg.OK()]]
+L_GO = [[psg.Frame('Output', [
+          [psg.Input(k='filename'),psg.Text('.png')],
+          [psg.Button('Create', k='go'), psg.Text(k='status')]
+       ])]]
 """UI elements for triggering generation."""
 L_INFO = [[psg.StatusBar('github.com/gavboi', justification='c')]]
 """UI elements for window information."""
 
 
-def main():
+def display_status(status):
+    """Updates status text in window.
 
-    layout = L_F1 + L_F2 + L_GO + L_INFO
-    
-    win = psg.Window('Get filename example', layout)
+    :param status: text to insert
+    :type status: str
+    """
+
+    win['status'].update(status)
+
+
+layout = L_F1 + L_F2 + L_GO + L_INFO
+win = psg.Window('Short APNG', layout)
+while True:
     event, values = win.read()
-    win.close()
+    if event in (psg.WIN_CLOSED, 'Exit'):
+        break
+    if event == 'go':
+        if not create.verify_image(values['f1']):
+            display_status('Image 1 not valid!')
+            continue
+        if not create.verify_image(values['f2']):
+            display_status('Image 2 not valid!')
+            continue
+        filename = 'out' if values['filename'] == '' else values['filename']
+        if (create.make_apng(values['f1'], values['f2'], 'out') if
+            filename == '' else
+            create.make_apng(values['f1'], values['f2'], filename)):
+                display_status(filename + '.png created.')
+        else:
+            display_status('File unable to be created.')
+win.close()
 
-if __name__ == '__main__':
-    main()
+
